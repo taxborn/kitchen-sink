ce05e8229c67342159326a15481a593ad3fa2b4d
Braxton Fair <hello@taxborn.com>
2024-01-30 21:58:13 -0600

    one-liner practice

--- array-chunking-python/chunk.py
+++ array-chunking-python/chunk.py
@@ -10,6 +10,12 @@ def chunking(input: list[int], chunk_size: int):
     return out
 
 """
+The above but as a one-liner
+"""
+def chunking_oneliner(input: list[int], chunk_size: int):
+    return [input[i:i+chunk_size] for i in range(0, len(input), chunk_size)]
+
+"""
 Method to chunk a list in python into a specified number of buckets. If the 
 input cannot be evenly divided, the extra will be put into an extra bucket.
 """
@@ -23,32 +29,33 @@ def chunking_even_size(input: list[int], buckets: int):
     return out
 
 """
+The above but as a one-liner, this really is ugly and hard to 
+read but I like trying
+"""
+def chunking_even_size_oneliner(input: list[int], buckets: int):
+    return [input[i:i + chunk_size] for i in range(0, len(input), len(input) // buckets)]
+
+"""
 Method to chunk a list in python into a specified number of buckets. If the 
 input cannot be evenly divided, the extra will be put into the last bucket
 """
 def chunking_even_size_forced(input: list[int], buckets: int):
-    chunk_size = len(input) // buckets
-    out = []
-
-    for i in range(0, len(input), chunk_size):
-        out.append(input[i:i + chunk_size])
+    out = chunking_even_size(input, buckets)
 
     # Check if we couldn't evenly divide
-    if len(out[-1]) != chunk_size:
+    if len(out[-1]) != len(input) // buckets:
         # Append the last bucket to the 2nd to last
         out[-2] += out[-1]
         # Remove the last bucket
-        out.pop(len(out)-1)
+        out.pop()
 
     return out
 
-def main():
+if __name__ == "__main__":
     my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
     print(chunking(my_list, 2))
+    assert chunking(my_list, 2) == chunking_oneliner(my_list, 2)
+
     print(chunking(my_list, 4))
     print(chunking_even_size(my_list, 3))
     print(chunking_even_size_forced(my_list, 3))
-
-
-if __name__ == "__main__":
-    main()
